d9f12d4bb334073f8c309df06beb6c279c036130
Aashish Kadel <kadelaashish22@gmail.com>
2022-10-07 23:11:50 +0545

    Some values changed

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ function App() {
   return (
     <main className="app">
       <div className="dice-container">
-        <Die value = "1"/>
         <Die value = "2"/>
+        <Die value = "2"/>
+        <Die value = "5"/>
         <Die value = "3"/>
-        <Die value = "4"/>
         <Die value = "5"/>
         <Die value = "6"/>
-        <Die value = "1"/>
+        <Die value = "5"/>
         <Die value = "2"/>
         <Die value = "3"/>
         <Die value = "5"/>
