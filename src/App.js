6c710df440423926e70f82fa4043251d74b00b75
Aashish Kadel <aashishkadel@Aashishs-MacBook-Pro.local>
2022-10-07 23:01:00 +0545

    initial commit

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Die value = "1"/>
         <Die value = "2"/>
         <Die value = "3"/>
-        <Die value = "4"/>
+        <Die value = "5"/>
       </div>
     </main>
   );
