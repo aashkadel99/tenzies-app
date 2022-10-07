2513ae0b77cd18159e417be0c6fc65380761ad90
Aashish Kadel <kadelaashish22@gmail.com>
2022-10-07 23:55:24 +0545

    Added button and other styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
+import React, { useState } from 'react';
 import './style.css';
 import Die from "./components/Die"
 
+
 function App() {
+
+  function allNewDice(){
+    const newDice = []
+    for(let i = 0; i < 10; i++){
+      newDice.push(Math.ceil(Math.random() * 6))
+    }
+    return newDice
+  }
+
+  const [loadDice, setLoadDice] = useState(allNewDice())
+
+  const allDice = loadDice.map((dice, index) => {
+    return (
+      <Die
+        key = {index}
+        value = {dice}
+      />
+    )
+  })
+
+  function rollNewSet(){
+    setLoadDice(allNewDice())
+  }
+
   return (
     <main className="app">
       <div className="dice-container">
-        <Die value = "2"/>
-        <Die value = "2"/>
-        <Die value = "5"/>
-        <Die value = "3"/>
-        <Die value = "5"/>
-        <Die value = "6"/>
-        <Die value = "5"/>
-        <Die value = "2"/>
-        <Die value = "3"/>
-        <Die value = "5"/>
+        {allDice}
       </div>
+      <button className = "roll-button" onClick = {rollNewSet}>Roll</button>
     </main>
   );
 }
 
-export default App;
+export default App; 
